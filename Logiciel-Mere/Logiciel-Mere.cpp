74680be44033c9c507ce2d8828603b709af41436
Gwenitora <gwenitora.gardu74@gmail.com>
2023-11-27 16:23:31 +0100

    debug: remove x64

--- Logiciel-Mere/Logiciel-Mere.cpp
+++ Logiciel-Mere/Logiciel-Mere.cpp
@@ -114,7 +114,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     PAINTSTRUCT ps;
     HDC hdc;
-    TCHAR greeting[] = _T("Hello, Windows desktop!");
+    TCHAR greeting[] = _T("Hello, Windows desktop!   ");
 
     switch (message)
     {
