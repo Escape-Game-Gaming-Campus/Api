0dc2082088fd35591cacd5c69315ee510081648b
ebeaufils <ebeaufils@GCFRLY232>
2023-11-28 13:28:24 +0100

    feature : password input

--- EscapeGame_GamingCampus/resource.h
+++ EscapeGame_GamingCampus/resource.h
@@ -13,6 +13,8 @@
 #define IDC_ESCAPEGAMEGAMINGCAMPUS      109
 #define IDM_LOGIN                       110
 #define IDR_MAINFRAME                   128
+#define IDC_GETUserName                 1001
+#define IDC_GETPASSWORD                 1002
 #define ID_AIDE_FORM                    32771
 #define ID_AIDE_TEST                    32774
 #define IDM_ESCAPEGAMEGAMINGCAMPUS_DIALOG 32775
@@ -23,9 +25,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32778
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
