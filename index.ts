06c97fc05510ab6bad56f184fd9d906c8c61098d
Gwenitora <gwenitora.gardu74@gmail.com>
2023-12-07 06:55:56 +0100

    init serv

--- index.ts
+++ index.ts
@@ -1,13 +1,32 @@
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
-  appId:"1717184",
-  key:"bc08e3d493ddd062e4b1",
-  secret:"d0ee021bf45c0babc607",
-  cluster:"eu",
-  useTLS:true
+  appId: "1720823",
+  key: "39f939b9f53716caf5d8",
+  secret: "201eb1f9e95745623191",
+  cluster: "eu",
+  useTLS: true
 });
 
-pusher.trigger("my-channel", "my-event", {
-  message:"hello world"
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(cors());
+
+app.get('/sendMessage', (req, res) => {
+    const data = { message: 'Hello from the API!' };
+
+    pusher.trigger("my-channel", "my-event", {
+      message: "hello world"
+    });
+
+    console.log("Pusher triggered");
+
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
