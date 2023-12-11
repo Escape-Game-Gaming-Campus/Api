944ca0336622fb227d1450ef1517e05cb239d90e
Gwenitora <gwenitora.gardu74@gmail.com>
2023-12-11 10:28:50 +0100

    Feature: better logged

--- commandManager/commandManager.ts
+++ commandManager/commandManager.ts
@@ -10,7 +10,20 @@ export default function CommandManager() {
         if(!file.endsWith(".js")) return;
         let cmd : Command = require("./commands/" + file)
         app[CommandType[cmd.type] as "get"](cmd.path, async (req, res) => {
+            const dateActuelle = new Date();
+            const options: Intl.DateTimeFormatOptions = {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+              timeZoneName: 'short'
+            };
+            const formatDate = new Intl.DateTimeFormat('fr-FR', options).format(dateActuelle);
+            
             cmd.run(req, res)
+            console.log(COLOR_TEXT.GRAY + "[" + formatDate.split(" ").join("-") + "] " + COLOR_TEXT.MAGENTA + res.statusCode + " " + COLOR_TEXT.CYAN + req.method + FORMAT_TEXT.RESET + " " + COLOR_TEXT.YELLOW + req.path + FORMAT_TEXT.RESET + " from " + COLOR_TEXT.GREEN + req.ip + FORMAT_TEXT.RESET)
         })
         console.log("  Added command '" + COLOR_TEXT.CYAN + cmd.name + FORMAT_TEXT.RESET + "'\n" + FORMAT_TEXT.UNDERSCORE + "type" + FORMAT_TEXT.RESET + ": " + COLOR_TEXT.MAGENTA + CommandType[cmd.type] + FORMAT_TEXT.RESET + "\n" + FORMAT_TEXT.UNDERSCORE + "desc" + FORMAT_TEXT.RESET + ": " + cmd.description + "\n" + FORMAT_TEXT.UNDERSCORE + "path" + FORMAT_TEXT.RESET + ": http://localhost:" + APPPort + cmd.path + "\n")
     })
