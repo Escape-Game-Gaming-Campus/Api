39aaa9c5e0867505148f53e6d3d2512a2b3610c9
Gwenitora <gwenitora.gardu74@gmail.com>
2023-12-13 14:50:31 +0100

    Feature: create channel Enigms on pusher

--- constants/pusherChannels.ts
+++ constants/pusherChannels.ts
@@ -1,6 +1,7 @@
 enum pusherChannels { // les tokens de channel pusher sont des string de 50 caractères aléatoires composer de chiffres et de lettres minuscules et majuscules
     DEV =                 "uotes6lA7qOE55AhIvgdRUa6B12LFRS1uQDxib7RpZiMjnZz3X",
-    INVENTORY =           "9GWL1MyLU96yFhAgthdjNphYUi51ADSsJfy77eBoLEGdpQvYIX"
+    INVENTORY =           "9GWL1MyLU96yFhAgthdjNphYUi51ADSsJfy77eBoLEGdpQvYIX",
+    ENIGMS =              "yRD2nbBLw3AjpS3ZQ8yLEgP5Mx557AISImcj6ZM35gvzEHG0Jt",
 }
 
 export default pusherChannels;
