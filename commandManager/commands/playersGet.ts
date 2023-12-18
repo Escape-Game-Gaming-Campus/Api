8a3e69abe4f6ead91121f7a66b8044165dd04ae3
Gwenitora <gwenitora.gardu74@gmail.com>
2023-12-18 12:38:27 +0100

    Debug: autoDoc

--- commandManager/commands/playersGet.ts
+++ commandManager/commands/playersGet.ts
@@ -4,32 +4,36 @@ import { Command } from "../command";
 import { getPlayers } from "../../utils/players";
 import Player from "../../constants/players";
 import pusherManager from "../../pusherManager/pusherManager";
+import { VarType } from "../../utils/doc";
 
 class PlayersGet extends Command {
   constructor() { super(); }
 
   public name: string = "Get players";
-  public description: string = "Get players";
+  public description: string = "Get tout les players ou un player en particulier";
   public path: string = "/players/get";
   public type: CommandType = CommandType.get;
+  public data: { ID: VarType, name: VarType } = { ID: {type: "number", description: "L'id du player à get", optional: true}, name: {type: "string", description: "Le nom du player à get", optional: true} };
+  public out: { player : [{ ID: VarType, name: VarType, position: VarType }], message: VarType } = { player: [{ ID: {type: "number", description: "L'id du player get", optional: false}, name: {type: "string", description: "le nom du player", optional: false}, position: {type: "number[]", description: "Liste de 3 valeurs tels un Vect3 correspondant au positions du joueur", optional: false} }], message: { type: "string", description: "message d'erreur/de succès", optional: false } };
 
   run(req: Request, res: Response) {
-    if (!req.body.ID)
+    pusherManager.executePusher("updatePlayers");
+    if (!req.body.ID || !req.body.name)
     {
-      pusherManager.executePusher("updatePlayers");
       res.status(200).json({players : getPlayers.sortByPLayeriD(), message: "Players get"});
+      return;
     }
     else 
     {
       getPlayers.sortByPLayeriD().forEach((el : Player) => {
-        if (el.ID ==req.body.ID)
+        if ((el.ID == req.body.ID && el.name == req.body.name) || (el.ID == req.body.ID && req.body.name === undefined) || (el.name == req.body.name && req.body.ID === undefined))
         {
-          pusherManager.executePusher("updatePlayers");
-          res.status(200).json({players : el, message: "Player get"});
+          res.status(200).json({players : [el], message: "Player get"});
           return;
         }
       })
     }
+    res.status(404).json({ message: "Player not found" });
   }
 }
 
--- commandManager/commands/playersUpdate.ts
+++ commandManager/commands/playersUpdate.ts
@@ -4,23 +4,26 @@ import { Command } from "../command";
 import pusherManager from "../../pusherManager/pusherManager";
 import { getPlayers } from "../../utils/players";
 import Player from "../../constants/players";
+import { VarType } from "../../utils/doc";
 
 class PlayersUpdate extends Command {
   constructor() { super(); }
 
   public name: string = "Update player";
-  public description: string = "Update player by name";
+  public description: string = "Met à jour un joueur (par son nom ou son id)";
   public path: string = "/players/update";
   public type: CommandType = CommandType.post;
+  public data: { players: [ {ID : VarType, name : VarType, position: VarType} ] } = { players: [{ ID: {type: "number", description: "L'id du player à détruire (facultatif si utilise le name)", optional: false}, name: {type: "string", description: "Le nom du player à détruire (facultatif si utilise l'id')", optional: false}, position: {type: "number[]", description: "3 nombres tels un vect3 qui définissent la position du joueur", optional: false} }] };
+  public out: { message: VarType } = { message: { type: "string", description: "message d'erreur/de succès", optional: false } };
 
   run(req: Request, res: Response) {
     var updated = false;
     if (!req.body.players) {
-      res.status(400).json({ success: false, message: "Missing parameter: players" })
+      res.status(400).json({ message: "Missing parameter: players" })
       return;
     }
     if (req.body.players.length === 0) {
-        res.status(400).json({ success: false, message: "Parameter players is empty" })
+        res.status(400).json({ message: "Parameter players is empty" })
         return;
       }
 
@@ -31,9 +34,9 @@ class PlayersUpdate extends Command {
 
     if (updated) {
       pusherManager.executePusher("updatePlayers");
-      res.status(200).json({ success: true, message: "Player updated" });
+      res.status(200).json({ message: "Player updated" });
     } else {
-      res.status(404).json({ success: false, message: "Player not found" });
+      res.status(404).json({ message: "Player not found" });
     }
   }
 }
